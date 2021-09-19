df188e382f37e3ddd622785652080b7db95b8f4b
Aaryan <aaryan14032006@gmail.com>
2021-09-19 13:24:59 +0530

    Created TabbedPane

---
+++ src/main/java/com/aryan1403/GUI/GUI.java
@@ -0,0 +1,25 @@
+package com.aryan1403.GUI;
+
+import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+
+public class GUI {
+    JFrame frame;
+
+    private final int frameX = 800;
+    private final int frameY = 700;
+    public GUI() {
+        JTabbedPane pane = new JTabbedPane();
+        pane.setBounds(10, 10, 790, 690);
+        pane.add("Insert Data", new InsertPane());
+        pane.setLayout(null);
+
+        frame.setSize(frameX, frameY);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(3);
+        frame.setResizable(false);
+        frame.setLayout(null);
+        frame.add(pane);
+        frame.setVisible(true);
+    }
+}
