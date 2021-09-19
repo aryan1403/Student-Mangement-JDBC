a8fa3df4e8a644775f1f67d56695ee1914ab50e3
Aaryan <aaryan14032006@gmail.com>
2021-09-19 13:12:50 +0530

    Created Student Class

---
+++ src/main/java/com/aryan1403/Manage/Student.java
@@ -0,0 +1,23 @@
+package com.aryan1403.Manage;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Data
+@Setter
+@Getter
+@ToString
+public class Student {
+    private int studentId;
+    private String studentName;
+    private String studentPhone;
+    private String studentCity;
+
+    public Student(String studentName, String studentPhone, String studentCity) {
+        this.studentName = studentName;
+        this.studentPhone = studentPhone;
+        this.studentCity = studentCity;
+    }   
+}
